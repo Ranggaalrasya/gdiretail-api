8c96aa4396beae493138975e51a3121bcb80e932
Arfa Naufal <arfaazizan.naufal@gmail.com>
2023-10-20 15:05:04 +0700

    feat:[AN] fix owen Product and Supplier Crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PurchaseModule } from './purchase/purchase.module';
 import { SaleModule } from './sale/sale.module';
+import { ProductModule } from './product/product.module';
+import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
-  imports: [PurchaseModule, SaleModule],
+  imports: [PurchaseModule, SaleModule, ProductModule, SupplierModule],
   controllers: [AppController],
   providers: [AppService],
 })
